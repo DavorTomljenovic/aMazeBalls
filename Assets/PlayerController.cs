eb9bf03424e03a2853efdcf655cd506ecd5fda96
DavorTomljenovic <tomljenovicd@yahoo.com>
2018-07-03 23:50:40 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        camera.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y + 5, rb.transform.position.z);
+    }
+}
